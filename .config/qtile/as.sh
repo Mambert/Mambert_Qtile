0d652df512b0326bd3e419d959465227f3a370f0
Mambert <87109751+Mambert@users.noreply.github.com>
2023-03-11 16:22:00 -0600

    Add files via upload

---
+++ .config/qtile/as.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+xrandr --output DisplayPort-0 --primary --mode 1920x1080 --pos 0x0 --rotate normal --refresh 144.0 --output DisplayPort-1 --off --output DisplayPort-2 --off --output HDMI-A-0 --off &
+
+nitrogen --restore &
+picom --config ~/.config/picom/picom.conf
+
